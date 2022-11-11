d3bbbc6964218090b237f2c5b38f47f1e0f869d9
lowc0ff <lowc0ff@protonmail.com>
2022-11-11 08:31:30 -0300

    index ok

--- my-project/src/index.js
+++ my-project/src/index.js
@@ -1,4 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+import App from './App';
 
 /**
  * This is the main entry point of the portlet.
@@ -9,6 +12,9 @@
  * @param  {Object} params a hash with values of interest to the portlet
  * @return {void}
  */
-export default function main({portletNamespace, contextPath, portletElementId, configuration}) {
-    
+ export default function main({portletNamespace, contextPath, portletElementId, configuration}) {
+	ReactDOM.render(
+		<App />,
+		document.getElementById(portletElementId)
+	);
 }
